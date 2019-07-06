4b0251dcd3180a6af985b2e35c570b7c2ccc846d
Dima Kynal <dimay90914@gmail.com>
2019-07-06 06:39:49 +0300

    N-значные числа

---
+++ 1-999/009.py
@@ -0,0 +1,24 @@
+# 9 N-значные числа
+n=int(input())
+def f(n):
+    if n==1:
+        print('10 0')
+    elif n==2:
+        print("1 22")
+    elif n==3:
+        print("6 123")
+    elif n==4:
+        print("12 1124")
+    elif n==5:
+        print("40 11125")
+    elif n==6:
+        print("30 111126")
+    elif n==7:
+        print("84 1111127")
+    elif n==8:
+        print("224 11111128")
+    elif n==9:
+        print("144 111111129")
+    elif n==10:
+        print()
+f(n)
