1e63da64165da9922ae397c9b3c31c07eab63e88
Dima Kynal <dimay90914@gmail.com>
2019-07-07 22:36:45 +0300

    Анаграммы

---
+++ 1-999/390.py
@@ -0,0 +1,13 @@
+# 390 Анаграммы
+from math import factorial
+n=input()
+v=set(n)
+k=0
+mass=[]
+for i in v:
+    k=n.count(i)
+    mass.append(k)
+rez=factorial(len(n))
+for i in mass:
+    rez/=factorial(i)
+print(int(rez))
