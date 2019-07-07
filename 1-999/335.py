d8a7d3312247dc74903752ca1ab61f42f44826a9
Dima Kynal <dimay90914@gmail.com>
2019-07-07 22:21:42 +0300

    Игра с переключателями

---
+++ 1-999/335.py
@@ -0,0 +1,11 @@
+# 335 Игра с переключателями
+q=int(input())
+mass=[0]*(100)
+for i in range(q):
+    n=int(input())
+    for j in range(1,n+1):
+        r=(n)**0.5
+    if (r == int(r)):
+        print(1)
+    else:
+        print(0)
