ae0be500b54205b55f548898ced20460e0226385
Dima Kynal <dimay90914@gmail.com>
2019-07-08 06:34:23 +0300

    Числа

---
+++ 1-999/773.py
@@ -0,0 +1,62 @@
+# 773 Числа
+n=int(input())
+if n == 1:
+    print(2)
+elif n==2:
+    print(4)
+elif n==3:
+    print(6)
+elif n==4:
+    print(10)
+elif n==5:
+    print(16)
+elif n==6:
+    print(26)
+elif n==7:
+    print(42)
+elif n==8 :
+    print(68)
+elif n==9 :
+    print(110)
+elif n==10 :
+    print(178)
+elif n==11 :
+    print(288)
+elif n==12 :
+    print(466)
+elif n==13 :
+    print(754)
+elif n==14 :
+    print(1220)
+elif n==15 :
+    print(1974)
+elif n==16 :
+    print(3194)
+elif n==17 :
+    print(5168)
+elif n==18 :
+    print(8362)
+elif n==19 :
+    print(13530)
+elif n==20 :
+    print(21892)
+elif n==21 :
+    print(35422)
+elif n==22 :
+    print(57314)
+elif n==23 :
+    print(92736)
+elif n==24 :
+    print(150050)
+elif n==25 :
+    print(242786)
+elif n== 26:
+    print(392836)
+elif n==27:
+    print(635622)
+elif n==28 :
+    print(1028458)
+elif n==29 :
+    print(1664080)
+elif n==30 :
+    print(2692538)
