c0d12ddf0f5bfab3f96b186de1e06d4a0f78fe63
Dima Kynal <dimay90914@gmail.com>
2019-07-08 20:45:21 +0300

    Уровень учебных достижений

---
+++ 1-999/902.py
@@ -0,0 +1,11 @@
+# 902 Уровень учебных достижений
+c=int(input())
+
+if 1<=c<=3:
+    print('Initial')
+elif 4<=c<=6:
+    print('Average')
+elif 7<=c<=9:
+    print('Sufficient')
+else:
+    print('High')
