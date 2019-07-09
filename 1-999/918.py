1fca68ead30629c01ffc2310f6e00f05b23141f1
Dima Kynal <dimay90914@gmail.com>
2019-07-09 05:46:06 +0300

    Какая четверть?

---
+++ 1-999/918.py
@@ -0,0 +1,14 @@
+# 918 Какая четверть?
+n1=input().split()
+x=float(n1[0])
+y=float(n1[1])
+if x>0 and y>0:
+    print('1')
+elif x<0 and y>0:
+    print('2')
+elif x<0 and y<0:
+    print('3')
+elif x==0 or y==0:
+    print('0')
+else:
+    print('4')
