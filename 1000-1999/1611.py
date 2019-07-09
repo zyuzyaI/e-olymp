2400a023e3d7582a9c0d5a5cdeb4fbca2d9e4a1e
Dima Kynal <dimay90914@gmail.com>
2019-07-09 22:17:37 +0300

    Реверс подстроки

--- 1000-1999/1611.py
+++ 1000-1999/1611.py
@@ -1,16 +1,6 @@
-# 1611 Измените единицу
-n=int(input())
-b=bin(n)
-c=b[::-1]
-k=0
-a=[]
-for i in c:
-    if i=='1':
-        k+=1
-        if k==1:
-            a.append('0')
-            continue
-    a.append(i)
-a=a[::-1]
-v=''.join(a)
-print(int(v,2))
+# 1611 Реверс подстроки
+n=input()
+ind=[int(i) for i in (input().split())]
+pod=n[ind[0]-1:ind[1]]
+spis=n[:ind[0]-1]+pod[::-1]+n[ind[1]:]
+print(spis)
