3dec3e29f31df94b4f932e182e200d8d403c2f80
Dima Kynal <dimay90914@gmail.com>
2019-07-11 05:38:36 +0300

    МаркЕрованные кубики

---
+++ 2000-2999/2803.py
@@ -0,0 +1,25 @@
+# 2803  МаркЕрованные кубики
+n=input()
+for i in n:
+    if i=='0':
+        print('1', end='')
+    elif i=='1':
+         print('2', end='')
+    elif i=='2':
+        print('3', end='')
+    elif i=='3':
+        print('4', end='')
+    elif i=='4':
+        print('5', end='')
+    elif i=='5':
+        print('6', end='')
+    elif i=='6':
+        print('7', end='')
+    elif i=='7':
+        print('8', end='')
+    elif i=='8':
+        print('9', end='')
+    elif i=='9':
+        print('0', end='')
+    else:
+        print(i,end='')
