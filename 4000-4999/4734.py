9a13218435656866c5d6161752bacc1169fec865
Dima Kynal <dimay90914@gmail.com>
2019-07-11 06:06:58 +0300

    Делится ли на 3, 6 и 9?

---
+++ 4000-4999/4734.py
@@ -0,0 +1,14 @@
+# 4734 Делится ли на 3, 6 и 9?
+n=int(input())
+if n%3==0:
+    print('Yes')
+else:
+    print('No')
+if n%6==0:
+    print('Yes')
+else:
+    print('No')
+if n%9==0:
+    print('Yes')
+else:
+    print('No')
