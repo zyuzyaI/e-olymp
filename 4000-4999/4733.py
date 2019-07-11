56408e13b98cccafeb75d36567a443713df5e1a9
Dima Kynal <dimay90914@gmail.com>
2019-07-11 06:05:53 +0300

    Делится ли на 2, 4 и 8?

---
+++ 4000-4999/4733.py
@@ -0,0 +1,14 @@
+# 4733 Делится ли на 2, 4 и 8?
+n=int(input())
+if n%2==0:
+    print('Yes')
+else:
+    print('No')
+if n%4==0:
+    print('Yes')
+else:
+    print('No')
+if n%8==0:
+    print('Yes')
+else:
+    print('No')
