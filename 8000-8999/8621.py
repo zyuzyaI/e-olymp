291a01d9289137e1e4a537264a89684afd9fa69e
Dima Kynal <dimay90914@gmail.com>
2019-07-14 19:42:49 +0300

    Две одинаковые цифры

--- 8000-8999/8621.py
+++ 8000-8999/8621.py
@@ -1,17 +1,14 @@
-# 8621 Делимость на цифры
+# 8621 Две одинаковые цифры
 n=int(input())
-a1=n//1000
-a2=(n//100)%10
-a3=(n//10)%10
-a4=n%10
-k=0
-if a1==0 or a2==0 or a3==0 or a4==0:
-    k=1
-else:
-    if n%a1!=0 or n%a2!=0 or n%a3 or n%a4!=0:
-        k=1
-
-if k==1:
-    print('NO')
-if k==0:
+a=n//1000
+b=(n//100)%10
+c=(n//10)%10
+d=n%10
+if a==c and b==d and a!=d:
     print('YES')
+elif a==b and c==d and a!=d:
+    print('YES')
+elif a==d and c==b and a!=b:
+    print('YES')
+else:
+    print('NO')
