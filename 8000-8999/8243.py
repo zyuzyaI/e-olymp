349fa0d88b7ac00252bbd625ccd308b98c3cc50f
Dima Kynal <dimay90914@gmail.com>
2019-07-14 18:43:41 +0300

    Первая цифра числа

---
+++ 8000-8999/8243.py
@@ -0,0 +1,10 @@
+# 8243 Первая цифра числа
+n=input()
+for i in n:
+    if i!='0' and i!='-' :
+        if n[0]=='-':
+            print(i)
+            break
+        else:
+            print(i)
+            break
