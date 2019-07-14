5aa95573cdb8bac7538edc8a7b8679c6956888d0
Dima Kynal <dimay90914@gmail.com>
2019-07-14 19:44:33 +0300

    Делимость на цифры

--- 8000-8999/8622.py
+++ 8000-8999/8622.py
@@ -1,15 +1,17 @@
-# 8622 Квадрат максимального
-n=input()
-k2=len(n)
-c=[]
-c1=[]
-n=int(n)
-for i in range(k2):
-    k1=n%10
-    n=n//10
-    c.append(k1)
-c.sort()
-c.reverse()
-a=('%d%d%d' %(c[0], c[1], c[2]))
-a=int(a)
-print(a**2)
+# 8622 Делимость на цифры
+n=int(input())
+a1=n//1000
+a2=(n//100)%10
+a3=(n//10)%10
+a4=n%10
+k=0
+if a1==0 or a2==0 or a3==0 or a4==0:
+    k=1
+else:
+    if n%a1!=0 or n%a2!=0 or n%a3 or n%a4!=0:
+        k=1
+
+if k==1:
+    print('NO')
+if k==0:
+    print('YES')
